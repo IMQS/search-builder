f2353ea45f98861f25e09ea50b25160d15eef7d4
Ben Harper <rogojin@gmail.com>
2015-01-28 10:59:55 +0200

    initial commit

---
+++ imqs-build.rb
@@ -0,0 +1,34 @@
+require "fileutils"
+
+out_dir = "../out"
+
+oldGoPath = ENV["GOPATH"]
+ENV["GOPATH"] = Dir.pwd
+
+at_exit {
+	ENV["GOPATH"] = oldGoPath
+}
+
+def exec_or_die(cmd, current_dir = nil)
+	orgDir = Dir.pwd
+	Dir.chdir(current_dir) if current_dir != nil
+	
+	res = `#{cmd}`
+	
+	Dir.chdir(orgDir)
+
+	if $?.exitstatus != 0
+		print(res)
+		exit(false)
+	end
+end
+
+case ARGV[0]
+	when "prepare"	then
+		exec_or_die("go install github.com/IMQS/search/cmd")
+		FileUtils.cp("bin/cmd.exe", out_dir + '/bin/imqs-search.exe')
+	when "test_unit" then
+		exec_or_die("go test github.com/IMQS/search/search")
+	when "test_integration" then
+end
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Search builder
+
+This is the top-level project for the Search service.
+
+This is basically just a bunch of git sub-module references and a build script.
