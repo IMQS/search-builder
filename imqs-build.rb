d633db9d29a9f6db4af5ffcccc34b2ed3d8f7f2f
Ben Harper <rogojin@gmail.com>
2016-12-05 14:21:30 +0200

    Add new copy_out build phase to imqs-build.rb

--- imqs-build.rb
+++ imqs-build.rb
@@ -26,6 +26,7 @@ end
 case ARGV[0]
 	when "prepare"	then
 		exec_or_die("go install github.com/IMQS/search/cmd")
+	when "copy_out" then
 		FileUtils.cp("bin/cmd.exe", out_dir + '/bin/imqssearch.exe')
 	when "test_unit" then
 		exec_or_die("go test github.com/IMQS/search/search -db_postgres -cpu 2")
